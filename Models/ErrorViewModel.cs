0106e540036b6d96efd547b4fa1c2b71b90e20f0
Hiago0021 <103226164+Hiago0021@users.noreply.github.com>
2022-11-16 20:47:04 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Barbearia_R7_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
